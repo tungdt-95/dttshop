8f56afc296fea4b54921e6a92dadaa0eec6d7f9a
tungdt-95 <doantrantung@gmail.com>
2021-06-22 20:06:50 +0700

    them slide

--- index.js
+++ index.js
@@ -3,3 +3,40 @@ $(document).ready(function(){
         $('ul').toggleClass('show')
     });
   });
+
+
+  //   SLIDE 
+
+document.addEventListener("DOMContentLoaded", function(){
+    // khai bao doi tuong can su dung
+    var btn = document.querySelectorAll(".next-page ul li");
+    var slides = document.querySelectorAll(".slide ul li");
+    //bat su kien click cho tung nut
+    for(var i=0 ; i<btn.length; i++)
+    {
+        btn[i].addEventListener("click",function(){
+            //xoa bo active
+            for(var i = 0 ; i <btn.length ; i++){
+                btn[i].classList.remove("active");
+            }
+           this.classList.add("active");
+           //het phan xu li chuyen mau cua btn (previousElementSibling )
+
+           //xu li tinh vi tri cua slide
+           var btnkh = this ;
+           var btnvt = 0;
+           for( btnvt = 0 ; btnkh = btnkh.previousElementSibling ; btnvt++)
+           {
+
+           }
+           //het voong lap nay bien i = stt
+
+           //cho slide an di = cach remove active
+           for (var i = 0; i< slides.length; i++) {
+              slides[i].classList.remove("active");
+              slides[btnvt].classList.add("active");
+           }
+        })
+    }
+
+})
